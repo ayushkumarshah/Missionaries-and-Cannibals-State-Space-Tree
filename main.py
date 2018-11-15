ce7e08989e395a131f0aa74601547685317f88dd
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2018-11-15 11:51:15 +0545

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ initial_state= [3,3,1]
 
 Node.num_of_instances=0
 t0=time()
-dfs=bfs(initial_state)
+solution=bfs(initial_state)
 t1=time()-t0
-print('Solution:', dfs)
+print('Solution:', solution)
 print('space:',Node.num_of_instances)
 print('time:',t1)
 
